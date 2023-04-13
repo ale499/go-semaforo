b89041268984c0a12b0e82ec0e39b316f7ef361b
lauti2311 <lautaro_ismael2010@hotmail.com>
2023-04-13 20:39:49 -0300

    Pruebas varias lauti

--- main.go
+++ main.go
@@ -7,6 +7,21 @@ func main() {
 	fmt.Println("Primer cambiogus")
 	fmt.Println("PRIMERA LINEA DE COMANDO PARA HACER EL PROYECTO QUE NOS DIJO EL PROFESOR RODRIGUEZ :D")
 
-	fmt.Print("Pruebas lauti 2")
+	fmt.Println("Pruebas lauti ")
+
+	var b int
+	var a int
+	var c int
+	b = 4
+
+	a = 5
+
+	c = a + b
+
+	fmt.Println("La suma es ", c)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i + 1)
+	}
 
 }
