2cf5a1cf69c6893dded5d5522d7b10ee205253cb
mxy1122 <leivaagustin178@gmail.com>
2023-04-19 10:58:31 -0300

    Second Changes: Maximo Bazan

--- main.go
+++ main.go
@@ -1,8 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Print("empezar")
-	fmt.Print("Primer cambiogus")
+
+	var n bool
+
+	fmt.Println("Comienzo:")
+	fmt.Println("PRIMERA LINEA DE COMANDO PARA HACER EL PROYECTO")
+
+	fmt.Println("Segunda Prueba")
+
+	fmt.Println("Funcionamiento basico de semaforos: ")
+
+	fmt.Print("Es una calle la cual es atrevesada por otra?")
+
+	fmt.Scan(&n)
+
+	for {
+		// Estado: Rojo
+		fmt.Println("Semaforo: Rojo")
+		time.Sleep(10 * time.Second) //Espera 10 segundos
+
+		// Estado: Amarillo
+		fmt.Println("Semaforo: Amarillo")
+
+		// Estado: Verde
+		time.Sleep(2 * time.Second) //Espera 2 segundos
+		fmt.Println("Semaforo: Verde")
+		time.Sleep(12 * time.Second) //Espera 12 segundos
+	}
+
 }
