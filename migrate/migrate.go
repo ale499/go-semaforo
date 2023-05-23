4378728cf57d80bdac1b92f7e6ea583a271f086f
lucascarrada3 <lucas.carrada@alumnos.frm.utn.edu.ar>
2023-05-23 16:37:09 -0300

    Modelado Base de Datos.

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,1 +1,15 @@
-package migrate
+package main
+
+import (
+	"github.com/TadeLauda/go-semaforo/initializers"
+	"github.com/TadeLauda/go-semaforo/models"
+)
+
+func init() {
+	initializers.Loadenv()
+	initializers.Conectdb()
+}
+
+func main() {
+	initializers.Db.AutoMigrate(&models.Semaforo{})
+}
