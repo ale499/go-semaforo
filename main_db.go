a05580090130938dd72f2fd0b05b24d816290385
Tadeo Laudadio <tadeolaudadio2003@gmail.com>
2023-05-23 12:44:57 -0300

    Creacion del servidor base

---
+++ main_db.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error cargando las variables de entorno")
+	}
+}
+
+func main() {
+	fmt.Println("Hola")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
